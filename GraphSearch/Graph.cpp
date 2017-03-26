030d9986a84e997ee9efdc040519c39f5153c9c5
Vivian Rajkumar <paintfrussianblue@gmail.com>
2017-03-26 12:55:15 +0800

    Bug Fixes Needed

--- GraphSearch/Graph.cpp
+++ GraphSearch/Graph.cpp
@@ -324,28 +324,28 @@ int main() {
 
 	// Graph 2
 	Graph g2(5);
-	g2.addEdge(0, 3); // 0 is S, 3 is C
-	g2.addEdge(0, 2); // 2 is B
-	g2.addEdge(3, 2);
-	g2.addEdge(2, 1); // 1 is A
+	g2.addEdge(0, 2); // 0 is S, 3 is C
+	g2.addEdge(0, 3); // 2 is B
 	g2.addEdge(1, 4); // 4 is G
+	g2.addEdge(2, 1); // 1 is A
+	g2.addEdge(3, 2);
 
 	g2.DFS(0, 4);
 	g2.BFS(0, 4);
 	cout << endl;
 
 	// Graph 3
-//	Graph g3(5);
-//	g3.addEdge(0, 3); // 0 is S, 3 is C
-//	g3.addEdge(0, 2); // 2 is B
-//	g3.addEdge(3, 1); // 1 is A
-//	g3.addEdge(3, 4); // 4 is G
-//	g3.addEdge(2, 1);
-//	g3.addEdge(2, 4);
-//
-//	g3.DFS(0, 4);
-//	g3.BFS(0, 4);
-//	cout << endl;
+	Graph g3(5);
+	g3.addEdge(0, 1); // 0 is S, 1 is B
+	g3.addEdge(0, 2); // 2 is C
+	g3.addEdge(1, 3); // 3 is A
+	g3.addEdge(1, 4); // 4 is G
+	g3.addEdge(2, 3);
+	g3.addEdge(3, 4);
+
+	g3.DFS(0, 4);
+	g3.BFS(0, 4);
+	cout << endl;
 
 	// Graph 4
 	Graph g4(9);
@@ -381,9 +381,9 @@ int main() {
 	g5.addEdge(3, 5, 7);
 	g5.addEdge(4, 5, 4);
 
-	g5.DFS(0, 5);
-	g5.BFS(0, 5);
-	g5.UCS(0, 5);
+//	g5.DFS(0, 5);
+//	g5.BFS(0, 5);
+//	g5.UCS(0, 5);
 	cout << endl;
 
 	return 0;
