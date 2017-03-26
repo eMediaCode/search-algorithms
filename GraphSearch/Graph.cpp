3e7d14632f5a1e9eac07095b6c22cf007e72fd33
Vivian Rajkumar <paintfrussianblue@gmail.com>
2017-03-26 09:41:02 +0800

    Bugs need to be fixed

--- GraphSearch/Graph.cpp
+++ GraphSearch/Graph.cpp
@@ -57,8 +57,23 @@ void Graph::DFSHelper(int start, int goal, bool visited[]) {
 	vector<int>::iterator i;
 	for(i = adjacent[start].begin(); i != adjacent[start].end(), visited[goal] != true; i++) {
 		if(!visited[*i]) {
-			cout << "->";
+			cout << " ";
 			DFSHelper(*i, goal, visited);
+		}
+	}
+}
+
+void Graph::DFSHelper(int start, int goal1, int goal2, bool visited[]) {
+	// Mark the current node as visited and print
+	visited[start] = true;
+	cout << start;
+
+	// Recur for all adjacent nodes, as long as goal has not yet been visited
+	vector<int>::iterator i;
+	for(i = adjacent[start].begin(); i != adjacent[start].end(), visited[goal1] != true, visited[goal2] != true; i++) {
+		if(!visited[*i]) {
+			cout << " ";
+			DFSHelper(*i, goal1, goal2, visited);
 		}
 	}
 }
@@ -77,6 +92,25 @@ void Graph::DFS(int start, int goal) {
 
 		// Call recursive helper function to print explored vertices
 		DFSHelper(start, goal, visited);
+	}
+
+	cout << endl;
+}
+
+void Graph::DFS(int start, int goal1, int goal2) {
+	if (start == goal1 || start == goal2) {
+		cout << start;
+		return;
+	}
+	else {
+		// Mark all nodes as not visited
+		bool *visited = new bool[size];
+		for(int i = 0; i < size; i++) {
+			visited[i] = false;
+		}
+
+		// Call recursive helper function to print explored vertices
+		DFSHelper(start, goal1, goal2, visited);
 	}
 
 	cout << endl;
@@ -107,6 +141,47 @@ void Graph::BFS(int start, int goal) {
 
 		// Check if goal has been visited
 		if (goal == start) {
+			cout << endl;
+			return;
+		}
+
+		// Get all adjacent vertices of dequeued node
+		// If adjacent has not been visited, mark visited
+		// and enqueue it
+		for(i = adjacent[start].begin(); i != adjacent[start].end(); ++i) {
+			if (!visited[*i]) {
+				visited[*i] = true;
+				queue.push_back(*i);
+			}
+		}
+	}
+}
+
+void Graph::BFS(int start, int goal1, int goal2) {
+	// Mark all nodes as not visited
+	bool *visited = new bool[size];
+	for(int i = 0; i < size; i++) {
+		visited[i] = false;
+	}
+
+	// Create queue
+	list<int> queue;
+
+	// Mark current node as visited, and enqueue
+	visited[start] = true;
+	queue.push_back(start);
+
+	// Iterator i used to get all adjacent nodes
+	vector<int>::iterator i;
+
+	while(!queue.empty()) {
+		// Dequeue a node and print it
+		start = queue.front();
+		cout << start << " ";
+		queue.pop_front();
+
+		// Check if goal has been visited
+		if (start == goal1 || start == goal2) {
 			cout << endl;
 			return;
 		}
@@ -165,6 +240,56 @@ void Graph::UCS(int start, int goal) {
 	}
 }
 
+void Graph::UCS(int start, int goal1, int goal2) {
+	int cur = 0;
+	int pos = 0;
+	priority_queue<priorityQueue,vector<priorityQueue>,comparison> pq;
+	priorityQueue s;
+
+	// Insert root into queue
+	s.path.push_back(start);
+	s.cost = 0;
+
+	vector<int>::iterator i;
+	pq.push(s);
+
+	// While queue is not empty
+	while (!pq.empty()) {
+		priorityQueue cPQ, tPQ;
+
+		// Dequeue max priority element from queue
+		cur = pq.top().path.back();
+		cPQ = pq.top();
+		pq.pop();
+
+		// If priorities are the same, choose alphabetically smaller path
+
+		// If path is ending in the goal state, print path and exit
+		if (cur == goal1 || cur == goal2) {
+			displayPath(cPQ);
+			exit(0);
+		}
+		else {
+			// Insert all children of the dequeued element, with cumulative costs as priority
+			for(i = adjacent[cur].begin(); i != adjacent[cur].end(); ++i) {
+				tPQ = cPQ;
+				tPQ.path.push_back(*i);
+				pos = find(adjacent[cur].begin(), adjacent[cur].end(), *i) - adjacent[cur].begin();
+				tPQ.cost += weight[cur].at(pos);
+				pq.push(tPQ);
+			}
+		}
+	}
+}
+
+void Graph::AStarTS(int start, int goal) {
+
+}
+
+void Graph::AStarGS(int start, int goal) {
+
+}
+
 void Graph::displayPath(struct priorityQueue p)
 {
   list<int>::iterator i;
@@ -176,7 +301,7 @@ void Graph::displayPath(struct priorityQueue p)
 
   for (i = p.path.begin(); i != p.path.end(); ++i)
   {
-    cout << "->" << *i;
+    cout << " " << *i;
   }
 	cout << endl;
 //  cout << "Path length: " << p.cost;
@@ -184,42 +309,82 @@ void Graph::displayPath(struct priorityQueue p)
 
 int main() {
 
-	// Create a graph
-	Graph g(4);
-	g.addEdge(0, 1);
-	g.addEdge(0, 2);
-	g.addEdge(1, 2);
-	g.addEdge(2, 0);
-	g.addEdge(2, 3);
-	g.addEdge(3, 3);
+	// Graph 1
+	Graph g1(5);
+	g1.addEdge(0, 1); // 0 is S, 1 is A
+	g1.addEdge(0, 2); // 2 is B
+	g1.addEdge(1, 2);
+	g1.addEdge(1, 4); // 4 is G
+	g1.addEdge(2, 3); // 3 is C
+	g1.addEdge(3, 4);
 
-	int startDFS = 2, goalDFS = 1, startBFS = 2, goalBFS = 1;
-
-	cout << "Depth-First Search (start node: " << startDFS << ", goal node: " << goalDFS << ")" << endl;
-	g.DFS(startDFS,goalDFS);
-	cout << endl;
-	cout << "Breadth-First Search (start node: " << startBFS << ", goal node: " << goalBFS << ")" << endl;
-	g.BFS(startBFS,goalBFS);
+	g1.DFS(0, 4);
+	g1.BFS(0, 4);
 	cout << endl;
 
-	// Create a graph with weights
-	Graph s(7);
-	s.addEdge(0, 1, 5);
-	s.addEdge(0, 3, 3);
-	s.addEdge(1, 2, 1);
-	s.addEdge(2, 4, 6);
-	s.addEdge(2, 6, 8);
-	s.addEdge(3, 4, 2);
-	s.addEdge(3, 5, 2);
-	s.addEdge(4, 1, 1);
-	s.addEdge(5, 6, 3);
-	s.addEdge(6, 4, 4);
+	// Graph 2
+	Graph g2(5);
+	g2.addEdge(0, 3); // 0 is S, 3 is C
+	g2.addEdge(0, 2); // 2 is B
+	g2.addEdge(3, 2);
+	g2.addEdge(2, 1); // 1 is A
+	g2.addEdge(1, 4); // 4 is G
 
-	int startUCS = 0, goalUCS = 6;
+	g2.DFS(0, 4);
+	g2.BFS(0, 4);
+	cout << endl;
 
-	cout << "Uniform-Cost-Search (start node: " << startUCS << ", goal node: " << goalUCS << ")" << endl;
-	s.UCS(startUCS, goalUCS);
+	// Graph 3
+//	Graph g3(5);
+//	g3.addEdge(0, 3); // 0 is S, 3 is C
+//	g3.addEdge(0, 2); // 2 is B
+//	g3.addEdge(3, 1); // 1 is A
+//	g3.addEdge(3, 4); // 4 is G
+//	g3.addEdge(2, 1);
+//	g3.addEdge(2, 4);
+//
+//	g3.DFS(0, 4);
+//	g3.BFS(0, 4);
+//	cout << endl;
 
+	// Graph 4
+	Graph g4(9);
+	g4.addEdge(0, 1, 1);
+	g4.addEdge(0, 2, 3);
+	g4.addEdge(1, 4, 2);
+	g4.addEdge(2, 4, 8);
+	g4.addEdge(2, 5, 5);
+	g4.addEdge(3, 1, 1);
+	g4.addEdge(3, 7, 4);
+	g4.addEdge(4, 3, 5);
+	g4.addEdge(4, 7, 14);
+	g4.addEdge(4, 8, 6);
+	g4.addEdge(5, 6, 1);
+	g4.addEdge(5, 8, 4);
+	g4.addEdge(6, 8, 2);
+	g4.addEdge(7, 8, 0);
+
+	g4.DFS(0, 7, 8);
+	g4.BFS(0, 7, 8);
+	g4.UCS(0, 7, 8);
+	cout << endl;
+
+	// Graph 5
+	Graph g5(6);
+	g5.addEdge(0, 1, 2);
+	g5.addEdge(0, 2, 1);
+	g5.addEdge(1, 2, 1);
+	g5.addEdge(1, 3, 3);
+	g5.addEdge(1, 4, 1);
+	g5.addEdge(2, 4, 5);
+	g5.addEdge(2, 5, 10);
+	g5.addEdge(3, 5, 7);
+	g5.addEdge(4, 5, 4);
+
+	g5.DFS(0, 5);
+	g5.BFS(0, 5);
+	g5.UCS(0, 5);
+	cout << endl;
 
 	return 0;
 }
--- GraphSearch/Graph.h
+++ GraphSearch/Graph.h
@@ -17,14 +17,20 @@ class Graph {
 	vector<int> *adjacent;
 	vector<int> *weight;
 	void DFSHelper(int node, int goal, bool visited[]);
+	void DFSHelper(int node, int goal1, int goal2, bool visited[]);
 public:
 	Graph(int size);
 	virtual ~Graph();
 	void addEdge(int node1, int node2);
 	void addEdge(int node1, int node2, int weight);
 	void DFS(int start, int goal);
+	void DFS(int start, int goal1, int goal2);
 	void BFS(int start, int goal);
+	void BFS(int start, int goal1, int goal2);
 	void UCS(int start, int goal);
+	void UCS(int start, int goal1, int goal2);
+	void AStarTS(int start, int goal);
+	void AStarGS(int start, int goal);
 	void displayPath(struct priorityQueue p);
 };
 
